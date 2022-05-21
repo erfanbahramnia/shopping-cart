bd79a1986ab2e8311090bdb4cf4d2dd7ff579737
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-21 19:19:50 +0430

    show amount of products that had been bought with cart context

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 
 // shop icon
 import shop from "../../assets/icons/shop.svg";
 // css module style
 import styles from "./Navbar.module.css";
+// context
+import { CartContext } from '../../contexts/CartContextProvider';
 
 const Navbar = () => {
+
+    const {state} = useContext(CartContext);
+
     return (
         <div className={styles.container}>
             <Link className={styles.homeLink} to="/">Products</Link>
             <div className={styles.cart}>
                 <Link to="/"><img src={shop} /></Link>
-                <span>0</span>
+                <span>{state.itemCounter}</span>
             </div>
         </div>
     );
