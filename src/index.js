dd77a556d412ea9e906054f334b26abe5c34a056
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-21 11:16:50 +0430

    universal style coded and imported to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 
+// css style
+import "./index.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
